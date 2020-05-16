cd21187b59d1a7297d9b85c26ec4b5fff97cacdf
Hao Y. Li <haoyang.li@ingrooves.com>
2020-05-16 08:40:00 -0700

    update: server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,17 @@
 import WebServer from "./lib/webServer";
 
 const whitelist = ["http://localhost:3001"];
-const corsOptions = {
-    origin: (origin: string | undefined, callback: Function) => {
-        if (origin && whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-};
-const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-
 const server = new WebServer({
-    corsOptions,
-    port,
+    corsOptions: {
+        origin: (origin: string | undefined, callback: Function) => {
+            if (origin && whitelist.indexOf(origin) !== -1) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+    },
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
     version: "/api/v1/",
 });
 
